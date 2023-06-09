c2a8bf43a41afb5056044ea4d1cba4c1136af2f8
Stina <38423842+Stina0081@users.noreply.github.com>
2023-06-09 14:01:49 +0200

    Update main.py
    trying to add connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import threading
+import socket
 
 def send_message():
     message = input_field.get()
@@ -8,7 +9,28 @@ def send_message():
 
     input_field.delete(0, tk.END)
 
-    print("Message sent:", message)
+    connection.send(message.encode())
+
+def receive_messages():
+    while True:
+        try:
+            message = connection.recv(1024).decode()
+           
+            chat_history.insert(tk.END, "Other: " + message + "\n")
+        except ConnectionAbortedError:
+            break
+
+def establish_connection():
+    global connection
+    host = host_entry.get()
+    port = int(port_entry.get())
+
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connection.connect((host, port))
+
+    
+    receive_thread = threading.Thread(target=receive_messages)
+    receive_thread.start()
 
 
 window = tk.Tk()
@@ -24,11 +46,23 @@ input_field = tk.Entry(window)
 input_field.pack(padx=10, pady=10, fill=tk.X)
 
 
-window.bind("<Return>", lambda event: send_message())
-
-
 send_button = tk.Button(window, text="Send", command=send_message)
 send_button.pack(pady=10)
+
+
+host_label = tk.Label(window, text="Host:")
+host_label.pack()
+host_entry = tk.Entry(window)
+host_entry.pack(padx=10, pady=5)
+
+port_label = tk.Label(window, text="Port:")
+port_label.pack()
+port_entry = tk.Entry(window)
+port_entry.pack(padx=10, pady=5)
+
+
+connect_button = tk.Button(window, text="Connect", command=establish_connection)
+connect_button.pack(pady=10)
 
 
 window.mainloop()
